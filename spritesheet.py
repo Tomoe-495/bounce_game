8150826a272aa1eb23fc9bee95bb8ce8bf799993
tomoe-495 <hasnainsiddique495@gmail.com>
2023-12-30 16:11:31 +0500

    os path on spritesheet

--- spritesheet.py
+++ spritesheet.py
@@ -1,9 +1,10 @@
 import pygame
+import os
 
 class Sprite:
     def __init__(self, filename):
         self.filename = filename
-        self.sheet = pygame.image.load(filename).convert_alpha()
+        self.sheet = pygame.image.load(os.path.join(filename)).convert_alpha()
     
     def get_sprite(self, pos, size):
         sprite = pygame.Surface((size, size))
