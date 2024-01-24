736b802f5cc4c04f1a2663f148095f728d6a3ff4
tomoe-495 <hasnainsiddique495@gmail.com>
2024-01-24 10:04:32 +0500

    water

--- water.py
+++ water.py
@@ -1,24 +1,27 @@
 import pygame
 
 class Pond:
-    def __init__(self, points:list, height=64):
+    def __init__(self, points:list, width, height):
         self.points = points
         self.pos = self.points[0][1]
-        self.acc = 1
+        self.acc = 0.8
+        height *= 3
+        self.points += [ [self.points[-1][0], self.points[0][1] + height], [self.points[0][0], self.points[0][1] + height] ]
+
 
     def draw(self, win, scroll):
         point = list(map(lambda x: (x[0] - scroll[0], x[1] - scroll[1]), self.points))
-        # pygame.draw.polygon(win, (72, 178, 223), point)
-        pygame.draw.lines(win, (72, 178, 223), False, point)
+        # pygame.draw.lines(win, (72, 178, 223), True, point)
+        pygame.draw.polygon(win, (72, 178, 223, 10), point)
         
     
     def update(self, ball):
-        for i, point in enumerate(self.points):
+        for i, point in enumerate(self.points[0:-2]):
             if ball.rect.collidepoint(point):
                 self.points[i][1] += ball.vel*0.8
 
     def rupple(self):
-        for i, point in enumerate(self.points):
+        for i, point in enumerate(self.points[0:-2]):
             if point[1] > self.pos:
                 point[1] = max(self.pos, point[1] - self.acc)
             elif point[1] < self.pos:
@@ -35,7 +38,7 @@ class Water:
         points = []
         for path in self.paths:
             pts = [[i, path["px"][1] + (16//2)] for i in range(path["px"][0], path["px"][0] + path["width"], 8)]
-            points.append(Pond(pts))
+            points.append(Pond(pts, path["width"], path["height"]))
         return points
 
     def draw(self, win, scroll):
